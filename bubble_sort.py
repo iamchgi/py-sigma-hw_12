619bb2e8e768a54d386221bd82a065cb5d3fc56b
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2024-11-30 18:33:13 +0200

    Sorting implemented

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,88 +1,24 @@
-
-
 """
-
 Бульбашковий алгоритм сортування
 
 Time Complexity: O(n^2)
 Auxiliary Space: O(1)
 """
+from busy_time_meter import busy_time_meter
 
-import time
-import random
-import cProfile
-from rand_example import show_result_image, random_uniform, np_sort, save_txt_file, data_str, random_uniform_int
 
+@busy_time_meter
 # Функція бульбашкового сортування
 def bubble_sort(arr):
     n = len(arr)
- 
     # Пройти по всіх елементах масиву
     for i in range(n):
  
-        # Останній та  i-й елементи вже на місці
+        # Останній та i-й елементи вже на місці
         for j in range(0, n-i-1):
 
             # Обхід масиву від 0 до n-i-1
             # Поміняти місцями, якщо знайдений елемент більший ніж наступний елемент
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-
     return arr
- 
- 
-# Запуск на виконання
-if __name__ == "__main__":
-
-    # ---------------------- вихідні параметри об'єкта сортування ----------------------------
-    a = 0
-    b = 10
-    iter = 10000
-    failename_start = 'start_value.txt'
-    failename_stop = 'stop_value.txt'
-
-    # ---------------------- сортування випадкового масиву float ---------------------------
-    random = random_uniform(a, b, iter)
-
-    # ---------------------- сортування випадкового масиву int ---------------------------
-    # random = random_uniform_int(a, b, iter)
-
-    save_txt_file(random, failename_start)                              # запис випадкового масиву у файл
-    show_result_image(random, 'random.uniform')                         # графік візуалізації вхідних не сортованих даних
-    random_sort = np_sort(random)                                       # сортування методами numpy
-    show_result_image(random_sort, 'random.uniform.sort')               # графік візуалізації сортованих даних
-    save_txt_file(random_sort, failename_stop)                          # запис сортованого масиву методами numpy у файл
-
-
-
-    # ---------------------- Аналітика складності алгоритму  ---------------------------
-    # Фіксація часу --------------------------
-    #
-    # StartTime = time.time()
-    # bubble_sort_arr = bubble_sort(random)                              # сортування бульбашкою
-    # totalTime = (time.time() - StartTime)
-    # print('totalTime =', totalTime, 's')
-
-    # Використання сервісів -------------------
-    cp = cProfile.Profile()                                             # аналітика складності
-    cp.enable()
-
-    bubble_sort_arr = bubble_sort(random)                               # сортування бульбашкою
-    cp.disable()
-    cp.print_stats()                                                    # аналітика складності
-
-    failename_stop = 'stop_value_bubble.txt'                            # запис сортованого масиву методом бульбашки у файл
-    save_txt_file(bubble_sort_arr, failename_stop)
-    show_result_image(bubble_sort_arr, 'random.uniform.sort.bubble')    # графік візуалізації сортованих за бульбашкою даних
-
-
-
-    # # ----------------- Аналітика складності - рівномірні дані ---------------
-    # items_number = 10000
-    # arr = [random.uniform(0, 99) for _ in range(items_number + 1)]
-    # cp = cProfile.Profile()
-    # cp.enable()
-    # bubble_sort(arr)
-    # cp.disable()
-    # cp.print_stats()
-
--- merge_sort.py
+++ merge_sort.py
@@ -3,73 +3,38 @@
 Time Complexity: O(n log(n))
 Auxiliary Space: O(n)
 """
+from busy_time_meter import busy_time_meter
 
-import cProfile
-import random
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-def mergeSort(arr):
-    if len(arr) > 1:
- 
-        # Знаходження середини масиву
-        mid = len(arr) // 2
- 
-        # Поділ елементів масиву
-        L = arr[:mid]
- 
-        # на 2 половини
-        R = arr[mid:]
- 
-        # Сортування першої половинки
-        mergeSort(L)
- 
-        # Сортування другої половинки
-        mergeSort(R)
- 
-        i = j = k = 0
- 
-        # Копіювати дані до тимчасових масивів L[] і R[]
-        while i < len(L) and j < len(R):
-            if L[i] <= R[j]:
-                arr[k] = L[i]
-                i += 1
-            else:
-                arr[k] = R[j]
-                j += 1
-            k += 1
- 
-        #Перевірка, чи залишився якийсь елемент
-        while i < len(L):
-            arr[k] = L[i]
+    # Ділимо масиви на дві частини
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+
+    # Злиття двох відвпорядкованих половин
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    sorted_array = []
+    i = j = 0
+
+    # Порівнюєм елементи і поєднуєм масиви
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_array.append(left[i])
             i += 1
-            k += 1
- 
-        while j < len(R):
-            arr[k] = R[j]
+        else:
+            sorted_array.append(right[j])
             j += 1
-            k += 1
 
-    return arr
+    # Додоєм елементи що залишилися
+    sorted_array.extend(left[i:])
+    sorted_array.extend(right[j:])
+    return sorted_array
 
-
-# Запуск на виконання
-if __name__ == '__main__':
-
-
-    # --------------- Демонстрація роботи ------------------
-    # arr = [12, 11, 13, 5, 6]
-    # print('Вхід: ', arr)
-    # size = len(arr)
-    # arr_out = mergeSort(arr)
-    # print('Вихід: ', arr_out)
-
-    # ----------------- Аналітика складності ---------------
-
-    cp = cProfile.Profile()
-    items_number = 1000000
-    arr = [random.uniform(0, 99) for _ in range(items_number + 1)]
-    size = len(arr)
-    cp.enable()
-    mergeSort(arr)
-    cp.disable()
-    cp.print_stats()
-
+@busy_time_meter
+def merge_sort_array(arr):
+    return merge_sort(arr)
